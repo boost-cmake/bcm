c2a743aecd94e176516d0081a81f315f9d6a16e3
Paul <pfultz2@yahoo.com>
2016-12-29 17:49:36 -0500

    Properly set version variable

--- share/bcm/cmake/BCMSetupVersion.cmake
+++ share/bcm/cmake/BCMSetupVersion.cmake
@@ -1,6 +1,11 @@
 
 set(BCM_HEADER_VERSION_TEMPLATE_FILE "${CMAKE_CURRENT_LIST_DIR}/version.hpp")
 
+macro(bcm_set_parent VAR)
+    set(${VAR} ${ARGN} PARENT_SCOPE)
+    set(${VAR} ${ARGN})
+endmacro()
+
 function(bcm_setup_version)
     set(options)
     set(oneValueArgs VERSION GENERATE_HEADER PARSE_HEADER PREFIX)
@@ -15,26 +20,27 @@ function(bcm_setup_version)
     endif()
 
     if(PARSE_VERSION)
-        set(PROJECT_VERSION ${PARSE_VERSION} PARENT_SCOPE)
-        set(${PROJECT_NAME}_VERSION ${PROJECT_VERSION} PARENT_SCOPE)
-        string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" ${PROJECT_NAME}_VERSION_MAJOR "${PROJECT_VERSION}" PARENT_SCOPE)
-        string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+.*" "\\1" ${PROJECT_NAME}_VERSION_MINOR "${PROJECT_VERSION}" PARENT_SCOPE)
-        string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" ${PROJECT_NAME}_VERSION_PATCH "${PROJECT_VERSION}" PARENT_SCOPE)
+        bcm_set_parent(PROJECT_VERSION ${PARSE_VERSION})
+        bcm_set_parent(${PROJECT_NAME}_VERSION ${PROJECT_VERSION})
+        string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" _version_MAJOR "${PROJECT_VERSION}")
+        string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+.*" "\\1" _version_MINOR "${PROJECT_VERSION}")
+        string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" _version_PATCH "${PROJECT_VERSION}")
         foreach(level MAJOR MINOR PATCH)
-            set(PROJECT_VERSION_${level} ${${PROJECT_NAME}_VERSION_${level}} PARENT_SCOPE)
+            bcm_set_parent(${PROJECT_NAME}_VERSION_${level} ${_version_${level}})
+            bcm_set_parent(PROJECT_VERSION_${level} ${_version_${level}})
         endforeach()
     elseif(PARSE_PARSE_HEADER)
         foreach(level MAJOR MINOR PATCH)
             file(STRINGS ${PARSE_PARSE_HEADER}
                          _define_${level}
-                         REGEX "#define ${PREFIX}_${level}_VERSION")
+                         REGEX "#define ${PREFIX}_VERSION_${level}")
             string(REGEX MATCH "([0-9]+)" _version_${level} "${_define_${level}}")
             # TODO: Check if it is empty
-            set(${PROJECT_NAME}_VERSION_${level} ${_version_${level}} PARENT_SCOPE)
-            set(PROJECT_VERSION_${level} ${${PROJECT_NAME}_VERSION_${level}} PARENT_SCOPE)
+            bcm_set_parent(${PROJECT_NAME}_VERSION_${level} ${_version_${level}})
+            bcm_set_parent(PROJECT_VERSION_${level} ${_version_${level}})
         endforeach()
-        set(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}" PARENT_SCOPE)
-        set(${PROJECT_NAME}_VERSION ${PROJECT_VERSION} PARENT_SCOPE)
+        bcm_set_parent(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
+        bcm_set_parent(${PROJECT_NAME}_VERSION ${PROJECT_VERSION})
     endif()
     # TODO: Get version from the project
 
